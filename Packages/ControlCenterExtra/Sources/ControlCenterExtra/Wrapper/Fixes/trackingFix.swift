6fd6da597acf15046c26a7b9c947c1147e9bdb7e
Sake <mail@sakesalverda.nl>
2025-07-13 14:29:58 +0200

    Fixes fullscreen issue where menubar would remain open after closing window

--- Packages/ControlCenterExtra/Sources/ControlCenterExtra/Wrapper/Fixes/trackingFix.swift
+++ Packages/ControlCenterExtra/Sources/ControlCenterExtra/Wrapper/Fixes/trackingFix.swift
@@ -9,31 +9,23 @@
 import SwiftUI
 
 fileprivate struct TrackingFixModifier: ViewModifier {
-    var menuDelegate: ControlCenterMenuDelegate?
+//    var menuDelegate: ControlCenterMenuDelegate?
 //    @Environment(\.isMenuPresented) private var isMenuPresented
 //    @Environment(\.scenePhase) private var scenePhase
     
     func body(content: Content) -> some View {
         content
-//            .onChange(of: scenePhase) { oldValue, newValue in
-//                print("SCENE PHASE CHANGE")
-//            }
-            .onChange(of: menuDelegate?.isPresented) { oldValue, newValue in
-            if oldValue == newValue { return }
-            print(oldValue, newValue)
-            if newValue == true {
-                print("STARTING MENU TRACKING")
+            .onAppear {
                 DistributedNotificationCenter.default().post(name: .beginMenuTracking, object: nil)
-            } else {
-                print("ENDING MENU TRACKING")
+            }
+            .onDisappear {
                 DistributedNotificationCenter.default().post(name: .endMenuTracking, object: nil)
             }
-        }
     }
 }
 
 extension View {
-    func trackingFix(menuDelegate: ControlCenterMenuDelegate) -> some View {
-        modifier(TrackingFixModifier(menuDelegate: menuDelegate))
+    func trackingFix() -> some View {
+        modifier(TrackingFixModifier())
     }
 }
